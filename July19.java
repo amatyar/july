ec99d1d04c54bd41bff1dc47a0d73b623c4c0204
amatyar@yahoo.com <amatyar@yahoo.com>
2023-07-19 23:35:59 -0400

    oUpdated

--- July19.java
+++ July19.java
@@ -6,7 +6,34 @@ public class July19
 	public static void main(String[] args) 
 	{
 		
-
+		HospitalBMC BMC = new HospitalBMC();
+		BMC.DeptStaff();
+		
+		HospitalBMC.Department  Dpt= BMC.new Department();
+		String ad= Dpt.totalMember();
+		System.out.println(ad);
 	}
 
+}
+class HospitalBMC
+{
+	int staff = 500;
+	String Dept = "Multi Department";
+	
+	class Department
+	{
+		String member;
+		
+		
+		public String totalMember()
+		{
+			return "Volunter";
+			
+		}
+	}
+	
+	public void DeptStaff() 
+	{
+		System.out.println(this.Dept +" has "+ this.staff);
+	}
 }
